918f99c5b43c7d6970586660a465122fc0a12199
Siddarth Kumar <siddarthkay@gmail.com>
2024-03-10 20:34:54 +0530

    use nftables set for better readability

--- nftables/nftables.go
+++ nftables/nftables.go
@@ -18,11 +18,6 @@ const (
 	envBackups = "backups"
 	envApp     = "app"
 	envLogs    = "logs"
-
-	portMySQLExporter = 9104
-	portMySQL         = 3306
-	portLogstash      = 5141
-	portNodeExporter  = 9100
 )
 
 func (n *Nftables) UpdateFirewallRules(services []consul.Service) error {
@@ -31,16 +26,12 @@ func (n *Nftables) UpdateFirewallRules(services []consul.Service) error {
 		return fmt.Errorf("failed to fetch existing rules: %v", err)
 	}
 
-	metricsAddresses := make(map[string]bool)
-	backupsAddresses := make(map[string]bool)
+	if err := n.createSets(); err != nil {
+		return err
+	}
 
-	for _, service := range services {
-		switch service.NodeMeta["env"] {
-		case envMetrics:
-			metricsAddresses[service.ServiceAddress] = true
-		case envBackups:
-			backupsAddresses[service.ServiceAddress] = true
-		}
+	if err := n.flushSets(); err != nil {
+		return err
 	}
 
 	for _, service := range services {
@@ -48,65 +39,79 @@ func (n *Nftables) UpdateFirewallRules(services []consul.Service) error {
 		address := service.ServiceAddress
 
 		switch env {
+		case envMetrics:
+			if err := n.addToSet("metrics_servers", address); err != nil {
+				return err
+			}
+		case envBackups:
+			if err := n.addToSet("backups_servers", address); err != nil {
+				return err
+			}
 		case envApp:
-			if err := n.applyAppRules(address, metricsAddresses, backupsAddresses, existingRules); err != nil {
+			if err := n.addToSet("app_servers", address); err != nil {
 				return err
 			}
 		case envLogs:
-			if err := n.applyLogstashRule(address, existingRules); err != nil {
-				return err
-			}
-		case envMetrics:
-			if err := n.applyNodeExporterRule(address, metricsAddresses, existingRules); err != nil {
+			if err := n.addToSet("logs_servers", address); err != nil {
 				return err
 			}
 		}
 	}
 
+	if err := n.applyRules(existingRules); err != nil {
+		return err
+	}
+
 	return nil
 }
 
-func (n *Nftables) applyAppRules(address string, metricsAddresses, backupsAddresses map[string]bool, existingRules map[string]bool) error {
-	for metricsAddress := range metricsAddresses {
-		rule := fmt.Sprintf("add rule filter INPUT ip saddr %s ip daddr %s tcp dport %d accept", metricsAddress, address, portMySQLExporter)
-		if err := n.applyRule(rule, existingRules); err != nil {
-			return fmt.Errorf("failed to apply MySQL exporter access rule: %v", err)
-		}
-	}
-	for backupsAddress := range backupsAddresses {
-		rule := fmt.Sprintf("add rule filter INPUT ip saddr %s ip daddr %s tcp dport %d accept", backupsAddress, address, portMySQL)
-		if err := n.applyRule(rule, existingRules); err != nil {
-			return fmt.Errorf("failed to apply MySQL access rule: %v", err)
-		}
-	}
-	return nil
-}
-
-func (n *Nftables) applyLogstashRule(address string, existingRules map[string]bool) error {
-	rule := fmt.Sprintf("add rule filter INPUT ip daddr %s tcp dport %d accept", address, portLogstash)
-	if err := n.applyRule(rule, existingRules); err != nil {
-		return fmt.Errorf("failed to apply logstash access rule: %v", err)
-	}
-	return nil
-}
-
-func (n *Nftables) applyNodeExporterRule(address string, metricsAddresses map[string]bool, existingRules map[string]bool) error {
-	if _, found := metricsAddresses[address]; found {
-		rule := fmt.Sprintf("add rule filter INPUT ip daddr %s tcp dport %d accept", address, portNodeExporter)
-		if err := n.applyRule(rule, existingRules); err != nil {
-			return fmt.Errorf("failed to apply node exporter access rule: %v", err)
-		}
-	}
-	return nil
-}
-
-func (n *Nftables) applyRule(rule string, existingRules map[string]bool) error {
-	if !existingRules[rule] {
-		output, err := n.executeCommand(strings.Fields(rule)...)
+func (n *Nftables) createSets() error {
+	sets := []string{"metrics_servers", "backups_servers", "app_servers", "logs_servers"}
+	for _, set := range sets {
+		output, err := n.executeCommand("add", "set", "filter", set, "{", "type", "ipv4_addr", "}")
 		if err != nil {
-			return fmt.Errorf("error applying rule '%s': %v, output: %s", rule, err, output)
+			return fmt.Errorf("failed to create set '%s': %v, output: %s", set, err, output)
 		}
 	}
+	return nil
+}
+
+func (n *Nftables) flushSets() error {
+	sets := []string{"metrics_servers", "backups_servers", "app_servers", "logs_servers"}
+	for _, set := range sets {
+		output, err := n.executeCommand("flush", "set", "filter", set)
+		if err != nil {
+			return fmt.Errorf("failed to flush set '%s': %v, output: %s", set, err, output)
+		}
+	}
+	return nil
+}
+
+func (n *Nftables) addToSet(set, element string) error {
+	output, err := n.executeCommand("add", "element", "filter", set, "{", element, "}")
+	if err != nil {
+		return fmt.Errorf("failed to add element '%s' to set '%s': %v, output: %s", element, set, err, output)
+	}
+	return nil
+}
+
+func (n *Nftables) applyRules(existingRules map[string]bool) error {
+	rules := []string{
+		"add rule filter INPUT ip saddr @metrics_servers ip daddr @app_servers tcp dport 9104 accept",
+		"add rule filter INPUT ip saddr @backups_servers ip daddr @app_servers tcp dport 3306 accept",
+		"add rule filter INPUT ip daddr @logs_servers tcp dport 5141 accept",
+		"add rule filter INPUT ip daddr @metrics_servers tcp dport 9100 accept",
+	}
+
+	for _, rule := range rules {
+		if !existingRules[rule] {
+			output, err := n.executeCommand(strings.Fields(rule)...)
+			if err != nil {
+				return fmt.Errorf("failed to apply rule '%s': %v, output: %s", rule, err, output)
+			}
+		}
+	}
+
 	return nil
 }
 
